39464674878ad13093a34ea3e70b148f6d9f5d03
German <futuregerm@gmail.com>
2020-04-08 12:57:38 -0600

    Updates

---
+++ precache-manifest.bfbc297ff5b004bdd76d7c6479901511.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20647e68542994b6c60cc9d4924a167d",
+    "url": "/color-palette-generator/index.html"
+  },
+  {
+    "revision": "15c34081b9c3ce18a175",
+    "url": "/color-palette-generator/static/css/main.9022fa2d.chunk.css"
+  },
+  {
+    "revision": "62725774eccb9ddefc8f",
+    "url": "/color-palette-generator/static/js/2.01fbdc69.chunk.js"
+  },
+  {
+    "revision": "15c34081b9c3ce18a175",
+    "url": "/color-palette-generator/static/js/main.564b34f4.chunk.js"
+  },
+  {
+    "revision": "d456ba6f32ecd9694982",
+    "url": "/color-palette-generator/static/js/runtime-main.fe927ca5.js"
+  }
+]);
